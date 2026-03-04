b4c6a6e7aa68bad3a17fcc95936562a8dd6e8ffd
Ethan <doank3442@gmail.com>
2026-03-04 16:09:47 +0700

    feat: Add special handling for `receivedEvent?.type == "1"` in `onJackpotAnimationComplete` to trigger confetti and invalidate queries without advancing the loop.

--- src/pages/audience-v1/page.tsx
+++ src/pages/audience-v1/page.tsx
@@ -270,7 +270,24 @@ export default function AudienceDeluxeV1() {
   };
   const onJackpotAnimationComplete = () => {
     // Chỉ xử lý khi đang trong trạng thái quay thật (tránh trigger lúc init)
-    // if (!isSpinning || !pendingWinner) return;
+    if (receivedEvent?.type == "1") {
+      // 2. Bắn pháo hoa
+      setTimeout(() => {
+        confetti({
+          particleCount: 200,
+          spread: 100,
+          origin: { y: 0.6 },
+          zIndex: 9999,
+        });
+      }, 1000);
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.CAMPAGIN.LIST_LUCKY_HISTORY],
+      });
+      // 4. Kết thúc trạng thái quay của lượt này
+      setIsSpinning(false);
+      return;
+    }
+    if (!isSpinning || !pendingWinner) return;
 
     // 2. Bắn pháo hoa
     setTimeout(() => {
@@ -281,9 +298,6 @@ export default function AudienceDeluxeV1() {
         zIndex: 9999,
       });
     }, 1000);
-    queryClient.invalidateQueries({
-      queryKey: [QUERY_KEY.CAMPAGIN.LIST_LUCKY_HISTORY],
-    });
     // 4. Kết thúc trạng thái quay của lượt này
     setIsSpinning(false);
     handleNextLoop();
